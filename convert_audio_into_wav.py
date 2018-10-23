0775c7181558ff40e32ae0ce8a164815ebded345
user name <user@domain.com>
2018-10-23 11:09:21 +0300

    Convert audio to np.int16

--- convert_audio_into_wav.py
+++ convert_audio_into_wav.py
@@ -1,12 +1,20 @@
 import librosa
 import sys
+import numpy as np
+from scipy.io import wavfile
 
 def convert_to_wav(path):
-	y, sr = librosa.core.load(path, sr=16000)
-	new_path = path.split('.')[0] + '.wav'
-	print("Exporting to ", new_path)
-	librosa.output.write_wav(new_path, y, sr)
+    '''
+    Requirements for Attack:
+        sample rate = 16000
+        data type   = np.int16
+    '''
+    y, sr = librosa.core.load(path, sr=16000)
+    y = y * 32767
+    new_path = path.split('.')[0] + '.wav'
+    print("Exporting to ", new_path)
+    wavfile.write('sample.wav', sr, y.astype(np.int16))
 
 if __name__ == '__main__':
-	assert len(sys.argv) > 1
-	convert_to_wav(sys.argv[1])
+    assert len(sys.argv) > 1
+    convert_to_wav(sys.argv[1])
